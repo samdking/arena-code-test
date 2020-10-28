e2b4833a58f5f898e78e2158583cc950a4053ea6
Sam King <sdking02@gmail.com>
2020-10-28 20:24:26 +0000

    Move parser file from lib into root

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 
 RSpec.describe 'parser' do
   it "is executable" do
-    expect { system %(./lib/parser.rb) }
+    expect { system %(./parser.rb) }
       .to output(a_string_including("it works!"))
       .to_stdout_from_any_process
   end
