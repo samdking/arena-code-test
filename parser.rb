9f567a8ed5c5d0abee0848d808c52d91ec9dccd8
Sam King <sdking02@gmail.com>
2020-10-28 22:31:44 +0000

    Tidy up parser.rb output and validate input

--- parser.rb
+++ parser.rb
@@ -3,20 +3,27 @@
 require_relative "lib/webpages_list"
 require_relative "lib/log_parser"
 
+if ARGV[0].nil?
+  raise "You must a file to parse as the first argument"
+end
+
 file_from_input = ARGV[0]
 file = LogParser.new(file_from_input)
 webpages_list = WebpagesList.new(file.import)
 
 # list of webpages with most page views ordered from most pages views to less page
 
+puts "list of webpages with most page views ordered from most pages views to less page views:"
+
 webpages_list.sort_highest_page_views.each do |line|
   puts "#{line.url} #{line.views} visits"
 end
+
+puts ""
+puts "list of webpages with most unique page views also ordered:"
 
 # list of webpages with most unique page views also ordered
 
 webpages_list.sort_unique_page_views.each do |line|
   puts "#{line.url} #{line.views} unique visits"
 end
-
-puts "it works!"
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,15 +1,25 @@
 require 'rspec'
 
 RSpec.describe 'parser' do
-  it "is executable" do
-    expect { system %(./parser.rb log/webserver.log) }
-      .to output
-      .to_stdout_from_any_process
+  context "when a valid file is passed as an argument" do
+    it "is executable" do
+      expect { system %(./parser.rb log/webserver.log) }
+        .to output
+        .to_stdout_from_any_process
+    end
+
+    it "runs without error" do
+      expect { system %(./parser.rb log/webserver.log) }
+        .not_to output
+        .to_stderr_from_any_process
+    end
   end
 
-  it "runs without error" do
-    expect { system %(./parser.rb log/webserver.log) }
-      .not_to output
-      .to_stderr_from_any_process
+  context "when no argument is passed" do
+    it "errors" do
+      expect { system %(./parser.rb) }
+        .to output
+        .to_stderr_from_any_process
+    end
   end
 end
