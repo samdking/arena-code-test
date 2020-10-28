7cbdfcfa93e36428111aa8e2caaecb84856984b5
Sam King <sdking02@gmail.com>
2020-10-28 22:23:57 +0000

    Build out parser spec

--- parser.rb
+++ parser.rb
@@ -1,3 +1,22 @@
 #!/usr/bin/env ruby
 
+require_relative "lib/webpages_list"
+require_relative "lib/log_parser"
+
+file_from_input = ARGV[0]
+file = LogParser.new(file_from_input)
+webpages_list = WebpagesList.new(file.import)
+
+# list of webpages with most page views ordered from most pages views to less page
+
+webpages_list.sort_highest_page_views.each do |line|
+  puts "#{line.url} #{line.views} visits"
+end
+
+# list of webpages with most unique page views also ordered
+
+webpages_list.sort_unique_page_views.each do |line|
+  puts "#{line.url} #{line.views} unique visits"
+end
+
 puts "it works!"
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,8 +2,14 @@ require 'rspec'
 
 RSpec.describe 'parser' do
   it "is executable" do
-    expect { system %(./parser.rb) }
-      .to output(a_string_including("it works!"))
+    expect { system %(./parser.rb log/webserver.log) }
+      .to output
       .to_stdout_from_any_process
+  end
+
+  it "runs without error" do
+    expect { system %(./parser.rb log/webserver.log) }
+      .not_to output
+      .to_stderr_from_any_process
   end
 end
