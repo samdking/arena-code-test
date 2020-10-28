a2c5b1a1693e882e52cadc61850876bd868271be
Sam King <sdking02@gmail.com>
2020-10-28 21:02:10 +0000

    Add sort_highest_page_views method to LogParser

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -30,9 +30,34 @@ RSpec.describe LogParser do
         expect { subject.import }.not_to raise_error
       end
 
-      it "returns each line" do
-        expect(subject.import.count).to eq 3
+      it "counts each line" do
+        expect(subject.import.count).to eq 5
       end
+    end
+  end
+
+  describe "#sort_highest_page_views" do
+    subject { LogParser.new(file) }
+
+    let(:file) { './spec/fixtures/file.log' }
+
+    it "runs" do
+      expect { subject.sort_highest_page_views }.not_to raise_error
+    end
+
+    it "only returns unique rows" do
+      expect(subject.sort_highest_page_views.count).to eq 3
+    end
+
+    it "returns the highest occuring value first, and its page views" do
+      expect(subject.sort_highest_page_views.first).to eq ['Baz', 2]
+    end
+
+    it "sorts alphabetically after page view" do
+      expect(subject.sort_highest_page_views[0..1]).to eq [
+        ['Baz', 2],
+        ['Foo', 2]
+      ]
     end
   end
 end
