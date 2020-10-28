e8dc4dce2f4de32885512f89ba871fbceee282e0
Sam King <sdking02@gmail.com>
2020-10-28 21:41:59 +0000

    Improve the specs

--- spec/webpages_list_spec.rb
+++ spec/webpages_list_spec.rb
@@ -3,32 +3,56 @@ require_relative '../lib/webpages_list'
 
 RSpec.describe WebpagesList do
   describe "#sort_highest_page_views" do
-    let(:contents) { [
-      'Bar 139.204.200.167',
-      'Foo 248.133.146.13',
-      'Baz 198.27.104.9',
-      'Baz 111.243.41.65',
-      'Foo 244.48.38.123'
-    ] }
-    subject { WebpagesList.new(contents) }
+    let(:contents) { [] }
+    subject { WebpagesList.new(contents).sort_highest_page_views }
 
-    it "runs" do
-      expect { subject.sort_highest_page_views }.not_to raise_error
+    context "when there is a single record" do
+      let(:contents) { [
+        'Bar 139.204.200.167',
+      ] }
+
+      it "returns the URL and its page views" do
+        expect(subject).to eq [["Bar", 1]]
+      end
     end
 
-    it "only returns unique rows" do
-      expect(subject.sort_highest_page_views.count).to eq 3
+    context "when there are 4 unique URLs" do
+      let(:contents) { [
+        'Bar 139.204.200.167',
+        'Foo 248.133.146.13',
+        'Baz 198.27.104.9',
+        'Fiz 244.48.38.123',
+      ] }
+
+      it "returns 4 URLs" do
+        expect(subject.count).to eq 4
+      end
+
+      it "returns them in alphabetical order" do
+        expect(subject.first.first).to eq "Bar"
+        expect(subject.last.first).to eq "Foo"
+      end
     end
 
-    it "returns the highest occuring value first, and its page views" do
-      expect(subject.sort_highest_page_views.first).to eq ['Baz', 2]
-    end
+    context "when there are duplicate URLs" do
+      let(:contents) { [
+        'Bar 139.204.200.167',
+        'Foo 248.133.146.13',
+        'Baz 198.27.104.9',
+        'Foo 244.48.38.123',
+      ] }
 
-    it "sorts alphabetically for matching page view" do
-      expect(subject.sort_highest_page_views[0..1]).to eq [
-        ['Baz', 2],
-        ['Foo', 2]
-      ]
+      it "only returns unique URLs" do
+        expect(subject.count).to eq 3
+      end
+
+      it "returns the URLs in order of occurences" do
+        expect(subject).to eq [
+          ["Foo", 2],
+          ["Bar", 1],
+          ["Baz", 1],
+        ]
+      end
     end
   end
 end
