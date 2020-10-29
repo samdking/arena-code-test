46dec7f0cc1d7fe5b10ba50abad05d63d8185ce5
Sam King <sdking02@gmail.com>
2020-10-29 10:18:51 +0000

    Refactor out some duplication in WebpagesList

--- lib/webpages_list.rb
+++ lib/webpages_list.rb
@@ -8,20 +8,23 @@ class WebpagesList
   end
 
   def sort_highest_page_views
-    contents
-      .map { |line| line.split(' ').first }
-      .group_by(&:itself)
-      .map { |url, occurences| [url, occurences.size] }
-      .sort_by { |k, v| [-v, k] }
-      .map { |k, v| PageView.new(k, v) }
+    contents_by_url
+      .map { |url, ips| PageView.new(url, ips.size) }
+      .sort_by { |page_view| [-page_view.views, page_view.url] }
   end
 
   def sort_unique_page_views
+    contents_by_url
+      .map { |url, ips| PageView.new(url, ips.uniq.size) }
+      .sort_by { |page_view| [-page_view.views, page_view.url] }
+  end
+
+  private
+
+  def contents_by_url
     contents
       .map { |line| line.split(' ') }
       .group_by { |url, ip| url }
-      .map { |url, occurences| [url, occurences.map(&:last).uniq.size] }
-      .sort_by { |k, v| [-v, k] }
-      .map { |k, v| PageView.new(k, v) }
+      .map { |url, views| [url, views.map(&:last)] }
   end
 end
